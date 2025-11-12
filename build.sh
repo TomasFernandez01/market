95c0614c262f539e6fbc461c986f6e2c494b5b04
Tomas Fernandez <tomas.jobs.gr@gmail.com>
2025-11-12 14:42:08 -0300

    Add pip install step to Render build script complete

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Render build script
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
 echo "Running migrations..."
 python manage.py migrate --noinput
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
